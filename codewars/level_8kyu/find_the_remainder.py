8a950bd175f62144d3165172f136c3327a64076c
KyeongHwanMin <h45652@naver.com>
2021-12-08 22:34:21 +0900

    feat:find_teh_remainder

--- codewars/level_8kyu/find_the_remainder.py
+++ codewars/level_8kyu/find_the_remainder.py
@@ -1,2 +1,24 @@
-def remainder(a,b):
-    #your code here
+# Mysolution
+def remainder(num1, num2):
+    if num1 < num2:
+        small, large = num1, num2
+
+    else:
+        small, large = num2, num1
+
+    if small == 0:
+        return None
+    return large % small
+
+# Best
+# def remainder(num1, num2):
+#     if min(num1, num2) == 0:
+#         return None
+#     elif num1 > num2:
+#         return num1 % num2
+#     else:
+#         return num2 % num1
+# Clever
+# def remainder(num1, num2):
+#     if min(num1, num2) != 0:
+#         return max(num1, num2) % min(num1, num2)

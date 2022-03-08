ccc437ac8078bc10868056cfe8beddddf6774c88
KyeongHwanMin <h45652@naver.com>
2022-03-08 21:23:31 +0900

    refactor: Difference of Volumes of Cuboids

--- codewars/level_8kyu/differenceofvolumesofcuboids.py
+++ codewars/level_8kyu/differenceofvolumesofcuboids.py
@@ -1,6 +1,9 @@
 # https://www.codewars.com/kata/58cb43f4256836ed95000f97/train/python
-from math import prod
-
+# from math import prod
+#
+#
+# def find_difference(a, b):
+#     return abs(prod(a) - prod(b))
 
 def find_difference(a, b):
-    return abs(prod(a) - prod(b))
+    return abs((a[1]*a[2]*a[0])-b[1]*b[2]*b[0])
